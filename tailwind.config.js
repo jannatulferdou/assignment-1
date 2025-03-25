e9b40b9eb10875c6e26bd2f354b5cef77e1ae315
jannatulferdou <jannatulfer2005@gmail.com>
2025-03-25 16:36:47 +0600

    fix code

---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+<nav>
+            <span style="margin-right: 100px;"><h1 class="title">Bangladesh 2.0</h1></span>
+          <span style="margin-left: 200px;"><a href=""><button class="btn">Sign In</button></a></span>
+        </nav>
